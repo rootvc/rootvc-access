a8a15364fb243e1ea48b0cc01119a25b4f1ae0d0
Lee Edwards <leeredwards@gmail.com>
2022-03-22 14:22:56 -0700

    Create Person records

--- server/routes/emails.js
+++ server/routes/emails.js
@@ -19,49 +19,64 @@ router.post('/emails', async (req, res) => {
     "messageId": body.messageId,
   };
 
+  _createMessage(data);
+  
   const owner = body.owner;
   const participants = [].concat(data.from, data.to, data.cc || [], data.replyTo || []);
-  const isFromOwner = data.from === owner;
-  const messageExists = await prisma.Message.findUnique({
-    where: { messageId: data.messageId }
-  }).then(res => res !== null);
 
-  // Create message, only if new messageId
-  const message = await prisma.Message.upsert({
+  participants.filter(p => p != owner).forEach(async (contact) => {
+    _createConnection(data, owner, contact);
+    _createPerson(contact);
+    // _enrich(contact);
+    // TODO: Enrich Person
+    // TODO: Insert Company
+  })
+
+  res.status(200).json({ message: `Processed new email: ${data.messageId}` });
+});
+
+const _createMessage = async (data) => { // Create message, only if new messageId
+  await prisma.Message.upsert({
     where: { messageId: data.messageId },
     update: {},
     create: data
   })
-  .then(res => { console.log('Message created: ', res); return res });
+  .then(res => { console.log('Message created: ', res) });
+};
+
+const _createConnection = async (data, owner, contact) => { // Create connection pair if doesn't exist, otherwise increment appropriate count
+  const isFromOwner = data.from === owner;
+
+  const existingMessage = await prisma.Message.findUnique({
+    where: { messageId: data.messageId }
+  }).then(res => res !== null);
   
-  // Process each participant in the email
-  participants.filter(p => p != owner).forEach(async (p) => {
-    // Create connection pair if doesn't exist, otherwise increment
-    var updateData = { toAndFromOwner: { increment: 1 } };
+  var updateData = { toAndFromOwner: { increment: 1 } };
     updateData[isFromOwner ? 'fromOwner' : 'toOwner'] = { increment: 1 };
 
     prisma.Connection.upsert({
       where: {
-        owner_contact: { owner: owner, contact: p },
+        owner_contact: { owner: owner, contact: contact },
       },
-      update: messageExists ? {} : updateData,
+      update: existingMessage === null ? {} : updateData,
       create: {
         owner: owner,
-        contact: p,
+        contact: contact,
         toOwner: isFromOwner ? 0 : 1,
         fromOwner: isFromOwner ? 1 : 0,
         toAndFromOwner: 1,
       },
     })
     .then(res => { console.log('Connection created/updated: ', res) });
+}
 
-    // TODO: Insert Person
-    // TODO: Enrich Person
-    // TODO: Insert Company
+const _createPerson = async (p) => { // Create person, only if new email
+  await prisma.Person.upsert({
+    where: { email: p },
+    update: {},
+    create: { email: p },
   })
-
-  res.status(200).json({ message: `Recorded email: ${message.messageId}` });
-
-});
+  .then(res => { console.log('Person created: ', res) });
+};
 
 module.exports = router;
