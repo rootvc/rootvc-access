de662893940b7fba657473464da246654af76c3b
Lee Edwards <leeredwards@gmail.com>
2022-06-16 01:06:38 -1000

    fix prod environment

--- pages/api/auth/[[...path]].js
+++ pages/api/auth/[[...path]].js
@@ -4,13 +4,15 @@ import supertokens from 'supertokens-node'
 import { backendConfig } from '../../../config/backendConfig'
 import NextCors from "nextjs-cors";
 
+const port = process.env.APP_PORT || 3000;
+
 supertokens.init(backendConfig());
 
 export default async function superTokens(req, res) {
   // NOTE: We need CORS only if we are querying the APIs from a different origin
   await NextCors(req, res, {
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "http://localhost:3000",
+    origin: process.env.NEXT_PUBLIC_APP_URL || `http://localhost${port}`,
     credentials: true,
     allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
   });
